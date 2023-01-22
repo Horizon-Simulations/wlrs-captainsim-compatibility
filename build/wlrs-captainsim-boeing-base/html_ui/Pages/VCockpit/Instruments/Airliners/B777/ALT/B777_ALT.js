364e1b820bb957c37ef30ba4ccb369cbc684edaf
Robin Breitfeld <robin01196@gmail.com>
2023-01-22 20:23:00 +0100

    remove build folder from source

--- build/wlrs-captainsim-boeing-base/html_ui/Pages/VCockpit/Instruments/Airliners/B777/ALT/B777_ALT.js
+++
@@ -1,5 +0,0 @@
-class B777_ALT extends Boeing_FCU.ALT {
-    get templateID() { return "B777_ALT"; }
-}
-registerInstrument("b777-alt-element", B777_ALT);
-//# sourceMappingURL=B747_8_ALT.js.map
--- build/wlrs-captainsim-boeing-base/html_ui/Pages/VCockpit/Instruments/Airliners/B777/HDG/B777_HDG.js
+++
@@ -1,5 +0,0 @@
-class B777_HDG extends Boeing_FCU.HDG {
-    get templateID() { return "B777_HDG"; } 
-}
-registerInstrument("b777-hdg-element", B777_HDG);
-//# sourceMappingURL=B747_8_HDG.js.map
