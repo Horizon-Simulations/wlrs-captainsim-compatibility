704a352f381fb89c315b40110d1bcf5bf0e2203c
thongteevee <168094139+thongteevee@users.noreply.github.com>
2024-06-04 14:35:11 -0400

    NEW: CUSTOM EFB FOR 777 LINEUP (777F first)

---
+++ wlrs-captainsim-boeing-base/html_ui/Pages/VCockpit/Instruments/Airliners/B777/EFB/B777_EFB.js
@@ -0,0 +1,14 @@
+class B777_EFB {
+    constructor() {
+    }
+    get templateID() { return "B777_EFB"; }
+
+
+    onUpdate(_deltaTime) {
+    }
+    onEvent(_event) {
+    }
+}
+
+registerInstrument("b777-efb", B777_EFB);
+//# sourceMappingURL=B777_EFB.js.map
