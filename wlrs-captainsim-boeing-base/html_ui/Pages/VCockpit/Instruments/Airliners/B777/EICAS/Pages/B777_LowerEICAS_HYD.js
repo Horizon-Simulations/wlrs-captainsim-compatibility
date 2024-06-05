c9cb12f3ba4d1e3cb15064bedd181811e5c6a7a3
thongteevee <168094139+thongteevee@users.noreply.github.com>
2024-06-05 12:42:13 -0400

    HYD page update

--- wlrs-captainsim-boeing-base/html_ui/Pages/VCockpit/Instruments/Airliners/B777/EICAS/Pages/B777_LowerEICAS_HYD.js
+++ wlrs-captainsim-boeing-base/html_ui/Pages/VCockpit/Instruments/Airliners/B777/EICAS/Pages/B777_LowerEICAS_HYD.js
@@ -4,6 +4,7 @@ var B777_LowerEICAS_HYD;
         constructor() {
             super();
             this.isInitialised = false;
+
         }
         get templateID() { return "B777LowerEICASHydTemplate"; }
         connectedCallback() {
@@ -11,15 +12,27 @@ var B777_LowerEICAS_HYD;
             TemplateElement.call(this, this.init.bind(this));
         }
         init() {
+            this.pressL = document.querySelector("#pressLvalue");
+            this.pressC = document.querySelector("#pressCvalue");
+            this.pressR = document.querySelector("#pressRvalue");
+            this.qtyL = document.querySelector("#qtyLvalue");
+            this.qtyC = document.querySelector("#qtyCvalue");
+            this.qtyR = document.querySelector("#qtyRvalue");
             this.isInitialised = true;
-            //setInterval(this.updateClock.bind(this), 1000);
-            // Call updateAPUData every 0.2 second
         }
 
         update(_deltaTime) {
+            this.updateHydraulic();
         }
 
         updateHydraulic() {
+            this.pressL.textContent = (SimVar.GetSimVarValue("HYDRAULIC PRESSURE:1", "psi")).toFixed(0);
+            this.pressC.textContent = (SimVar.GetSimVarValue("HYDRAULIC PRESSURE:2", "psi")).toFixed(0);
+            this.pressR.textContent = (SimVar.GetSimVarValue("HYDRAULIC PRESSURE:3", "psi")).toFixed(0);
+
+            this.qtyL.textContent = ((SimVar.GetSimVarValue("HYDRAULIC RESERVOIR PERCENT:1", "percent"))/100).toFixed(2);
+            this.qtyC.textContent = ((SimVar.GetSimVarValue("HYDRAULIC RESERVOIR PERCENT:2", "percent"))/100).toFixed(2);
+            this.qtyR.textContent = ((SimVar.GetSimVarValue("HYDRAULIC RESERVOIR PERCENT:3", "percent"))/100).toFixed(2);
         }
 
     }
