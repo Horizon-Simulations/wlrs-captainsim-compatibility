776fc916578e745cae2213bbc7d565488c332335
thongteevee <thongtv2006@gmail.com>
2024-06-14 23:40:55 -0400

    add HYD page elements and logic 1

--- wlrs-captainsim-boeing-base/html_ui/Pages/VCockpit/Instruments/Airliners/B777/EICAS/Pages/B777_LowerEICAS_HYD.js
+++ wlrs-captainsim-boeing-base/html_ui/Pages/VCockpit/Instruments/Airliners/B777/EICAS/Pages/B777_LowerEICAS_HYD.js
@@ -5,6 +5,9 @@ var B777_LowerEICAS_HYD;
             super();
             this.isInitialised = false;
 
+            this.lineL_ENG = new Array();
+            this.lineL_ELEC = new Array();
+
         }
         get templateID() { return "B777LowerEICASHydTemplate"; }
         connectedCallback() {
@@ -18,11 +21,35 @@ var B777_LowerEICAS_HYD;
             this.qtyL = document.querySelector("#qtyLvalue");
             this.qtyC = document.querySelector("#qtyCvalue");
             this.qtyR = document.querySelector("#qtyRvalue");
+
+            this.demandLeft = false;
+
+            //document.querySelector("#L_ELEC_PUMP")
+
+            //add elements
+            this.lineL_ENG.push(document.querySelector("#L_ENG_ACTIVE1"));
+            this.lineL_ENG.push(document.querySelector("#L_ENG_ACTIVE2"));
+            this.lineL_ELEC.push(document.querySelector("#L_ELEC_ACTIVE1"));
+            this.lineL_ELEC.push(document.querySelector("#L_ELEC_ACTIVE2"));
+            
+
+            //initial hide
+            for(var i = 0; i < this.lineL_ENG.length; i++) {
+                this.lineL_ENG[i].setAttribute("visibility", "hidden");
+            }
+            for(var i = 0; i < this.lineL_ENG.length; i++) {
+                this.lineL_ELEC[i].setAttribute("visibility", "hidden");
+            }
+            document.querySelector("#L_SHARE1").setAttribute("visibility", "hidden");
+            document.querySelector("#L_SHARE2").setAttribute("visibility", "hidden");
+            document.querySelector("#L_ENG_PUMP").setAttribute("visibility", "hidden");
+            document.querySelector("#L_ELEC_PUMP").setAttribute("visibility", "hidden");
+
+            setInterval(this.updateHydraulic.bind(this), 200);
             this.isInitialised = true;
         }
 
         update(_deltaTime) {
-            this.updateHydraulic();
         }
 
         updateHydraulic() {
@@ -33,6 +60,63 @@ var B777_LowerEICAS_HYD;
             this.qtyL.textContent = ((SimVar.GetSimVarValue("HYDRAULIC RESERVOIR PERCENT:1", "percent"))/100).toFixed(2);
             this.qtyC.textContent = ((SimVar.GetSimVarValue("HYDRAULIC RESERVOIR PERCENT:3", "percent"))/100).toFixed(2);
             this.qtyR.textContent = ((SimVar.GetSimVarValue("HYDRAULIC RESERVOIR PERCENT:2", "percent"))/100).toFixed(2);
+
+            this.updateLogicSwitches();
+            this.updateLogicKnobs();
+        }
+
+        updateLogicSwitches() {
+            //handles ON position for engine driven pumps (only when engine running)
+            if (SimVar.GetSimVarValue("A:HYDRAULIC SWITCH:1", "bool") && SimVar.GetSimVarValue("A:TURB ENG N1:1", "percent") > 2) {
+                //turn on when circuit on
+                document.querySelector("#L_ENG_PUMP").setAttribute("visibility", "visible");
+                if (SimVar.GetSimVarValue("A:HYDRAULIC PRESSURE:1", "psi") > 2200) {
+                    for(var i = 0; i < this.lineL_ENG.length; i++) {
+                        this.lineL_ENG[i].setAttribute("visibility", "visible");   
+                    }
+                }
+            }
+            else{
+                for(var i = 0; i < this.lineL_ENG.length; i++) {
+                    this.lineL_ENG[i].setAttribute("visibility", "hidden");
+                    document.querySelector("#L_ENG_PUMP").setAttribute("visibility", "hidden");
+                }
+            }
+
+
+        }
+
+        updateLogicKnobs() {
+            //handles ON position
+            if (SimVar.GetSimVarValue("L:XMLVAR_HYDRAULICS_DEMAND_LEFT", "Number") == 2) {
+                for(var i = 0; i < this.lineL_ELEC.length; i++) {
+                    this.demandLeft = true;
+                    this.lineL_ELEC[i].setAttribute("visibility", "visible");
+                    document.querySelector("#L_ELEC_PUMP").setAttribute("visibility", "visible");
+                    SimVar.GetSimVarValue("K:HYDRAULIC_SWITCH_TOGGLE", "Number", 1);
+                }
+            }
+
+            //handles OFF position
+            if (SimVar.GetSimVarValue("L:XMLVAR_HYDRAULICS_DEMAND_LEFT", "Number") == 0) {
+                for(var i = 0; i < this.lineL_ELEC.length; i++) {
+                    this.demandLeft = false;
+                    this.lineL_ELEC[i].setAttribute("visibility", "hidden");
+                    document.querySelector("#L_ELEC_PUMP").setAttribute("visibility", "hidden");
+                }
+            }
+
+
+            //handle share elements
+            if (this.demandLeft || (SimVar.GetSimVarValue("A:HYDRAULIC PRESSURE:1", "psi") > 2200))
+            {
+                document.querySelector("#L_SHARE1").setAttribute("visibility", "visible");
+                document.querySelector("#L_SHARE2").setAttribute("visibility", "visible");
+            }
+            else {
+                document.querySelector("#L_SHARE1").setAttribute("visibility", "hidden");
+                document.querySelector("#L_SHARE2").setAttribute("visibility", "hidden");
+            }
         }
 
     }
