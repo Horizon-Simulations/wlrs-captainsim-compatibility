58103a3b440fe778fbeab682244c5739d08978c8
thongteevee <thongtv2006@gmail.com>
2024-06-22 16:20:49 -0400

    slowly migrate to B777DataStore

--- wlrs-captainsim-boeing-base/html_ui/Pages/RobSim/SaltyDataStore.js
+++ wlrs-captainsim-boeing-base/html_ui/Pages/RobSim/SaltyDataStore.js
@@ -13,3 +13,17 @@ class SaltyDataStore {
         SetStoredData(`Salty747_${key}`, val);
     }
 }
+
+class B777DataStore {
+    static get(key, defaultVal) {
+        const val = GetStoredData(`B777_${key}`);
+        if (!val) {
+            return defaultVal;
+        }
+        return val;
+    }
+
+    static set(key, val) {
+        SetStoredData(`B777_${key}`, val);
+    }
+}
