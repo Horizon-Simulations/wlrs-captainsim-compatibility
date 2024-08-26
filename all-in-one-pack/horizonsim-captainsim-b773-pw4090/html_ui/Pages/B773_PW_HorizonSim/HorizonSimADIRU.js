4553fbc76124b87e068a7ca20d054d5483dae41f
thongteevee <thongtv2006@gmail.com>
2024-08-26 13:53:30 -0400

    replace HS IRS with HS ADIRU

--- all-in-one-pack/horizonsim-captainsim-b773-pw4090/html_ui/Pages/B773_PW_HorizonSim/HorizonSimADIRU.js
+++ all-in-one-pack/horizonsim-captainsim-b773-pw4090/html_ui/Pages/B773_PW_HorizonSim/HorizonSimADIRU.js
@@ -1,23 +1,26 @@
-class HorizonSimIRS {
+class HorizonSimADIRU {
     //https://www.youtube.com/watch?v=1JyzVI5DX5E
     constructor() {
     }
     init() {
         this.resetInitalTimer = 30;       //reset duration
-        this.irsTimer = 7 * 60;     //implement a better IRS logic later
+        this.adiruTimer = 7 * 60;     //implement a better ADIRU logic later
         this.currentPhase = SimVar.GetSimVarValue("SIM ON GROUND", "Bool");
         this.hasTO = false;
         this.hasLanded = false;
+
     }
     update() {
         if (!electricityIsAvail) {
-            this.irsTimer = 7 * 60;
+            this.adiruTimer = 7 * 60;
             this.resetInitalTimer = 30;
         }
     }
     update(electricityIsAvail) {
         // Calculate deltatime
         var timeNow = Date.now();
+        SimVar.SetSimVarValue("L:B777_Plane_Landed", "Bool", this.hasLanded);
+        SimVar.SetSimVarValue("L:B777_Plane_TookOff", "Bool", this.hasTO);
 
         if (this.lastTime == null) this.lastTime = timeNow;
         var deltaTime = timeNow - this.lastTime;
@@ -26,27 +29,27 @@ class HorizonSimIRS {
         //ADIRU handler logic
         const onGround = SimVar.GetSimVarValue("SIM ON GROUND", "Bool");
         const adiruButtonState = SimVar.GetSimVarValue("L:B777_ADIRU_BUTTON_STATE", "Bool");
-        const irsState = SimVar.GetSimVarValue("L:B777_IRS_STATE", "Enum");
+        const adiruState = SimVar.GetSimVarValue("L:B777_ADIRU_STATE", "Enum");
         const enginesRunning = SimVar.GetSimVarValue("ENG COMBUSTION:1", "Bool") || SimVar.GetSimVarValue("ENG COMBUSTION:2", "Bool");
 
         if (onGround && !adiruButtonState) {
             if (this.hasLanded && !enginesRunning) {
-                SimVar.SetSimVarValue("L:B777_IRS_STATE", "Enum", 0);
+                SimVar.SetSimVarValue("L:B777_ADIRU_STATE", "Enum", 0);
                 this.resetInitalTimer = 30;
             }
             else if (!this.hasTO && !this.hasLanded) {
-                SimVar.SetSimVarValue("L:B777_IRS_STATE", "Enum", 0);
+                SimVar.SetSimVarValue("L:B777_ADIRU_STATE", "Enum", 0);
                 this.resetInitalTimer = 30;
             }
-        } else if (!onGround && !adiruButtonState && irsState == 2) {
-            SimVar.SetSimVarValue("L:B777_IRS_STATE", "Enum", 2);
-        } else if (adiruButtonState && irsState != 2) {
+        } else if (!onGround && !adiruButtonState && adiruState == 2) {
+            SimVar.SetSimVarValue("L:B777_ADIRU_STATE", "Enum", 2);
+        } else if (adiruButtonState && adiruState != 2) {
             //initial/reset
             if (this.resetInitalTimer > 0) {
                 this.resetInitalTimer -= deltaTime / 1000;
             }
             else {
-                SimVar.SetSimVarValue("L:B777_IRS_STATE", "Enum", 1);
+                SimVar.SetSimVarValue("L:B777_ADIRU_STATE", "Enum", 1);
             }
         }
         
@@ -65,28 +68,25 @@ class HorizonSimIRS {
         //align ADIRU
         if (!electricityIsAvail) return;
 
-        var IRSState = SimVar.GetSimVarValue("L:B777_IRS_STATE", "Enum");
-
-        if (IRSState == 0) {
-            this.irsTimer = 7 * 60;
+        if (adiruState == 0) {
+            this.adiruTimer = 7 * 60;
         }
 
-        if (IRSState == 1) {
-            if (this.irsTimer > 0) {
-                this.irsTimer -= deltaTime / 1000;
-                if (this.irsTimer <= 0) {
-                    this.irsTimer = -1;
-                    SimVar.SetSimVarValue("L:B777_IRS_STATE", "Enum", 2);
+        if (adiruState == 1) {
+            if (this.adiruTimer > 0) {
+                this.adiruTimer -= deltaTime / 1000;
+                if (this.adiruTimer <= 0) {
+                    this.adiruTimer = -1;
+                    SimVar.SetSimVarValue("L:B777_ADIRU_STATE", "Enum", 2);
                 }
             }
         }
 
         //fail state
-        if (IRSState == -1) {
+        if (adiruState == -1) {
             
         }
 
-        SimVar.SetSimVarValue("L:B777_IRS_TIME_LEFT", "Enum", this.irsTimer);
-
+        SimVar.SetSimVarValue("L:B777_ADIRU_TIME_LEFT", "Enum", this.adiruTimer);
     }
 }
