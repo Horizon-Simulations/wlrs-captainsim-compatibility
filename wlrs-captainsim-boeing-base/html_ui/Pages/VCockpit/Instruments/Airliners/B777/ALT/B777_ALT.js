8ff8f9a7edccf96c7d699bcf244ae6e981fffa03
thongteevee <thongtv2006@gmail.com>
2024-08-28 10:26:58 -0400

    remove old wlrs base

--- wlrs-captainsim-boeing-base/html_ui/Pages/VCockpit/Instruments/Airliners/B777/ALT/B777_ALT.js
+++
@@ -1,4 +0,0 @@
-class B777_ALT extends Boeing_FCU.ALT {
-    get templateID() { return "B777_ALT"; }
-}
-registerInstrument("b777-alt-element", B777_ALT);
--- wlrs-captainsim-boeing-base/html_ui/Pages/VCockpit/Instruments/Airliners/B777/HDG/B777_HDG.js
+++
@@ -1,4 +0,0 @@
-class B777_HDG extends Boeing_FCU.HDG {
-    get templateID() { return "B777_HDG"; } 
-}
-registerInstrument("b777-hdg-element", B777_HDG);
